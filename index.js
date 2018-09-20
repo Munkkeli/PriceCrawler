487141ab0828381b4256803d9de91211d88c1acc
Tuomas Pöyry <tuomas@munkkeli.io>
2018-09-20 12:01:34 +0300

    Added project files

---
+++ index.js
@@ -0,0 +1,111 @@
+const watchlist = require('./watchlist.json');
+const Crawler = require('crawler');
+const TelegramBot = require('node-telegram-bot-api');
+const fs = require('fs');
+
+require('dotenv').config();
+
+let history = {};
+let lastHistory = null;
+
+const bot = new TelegramBot(process.env.TELEGRAM_BOT_KEY, { polling: false });
+
+const settings = {
+  verkkokauppa: {
+    baseUrl: 'https://www.verkkokauppa.com/fi/product/',
+    parsePrice: (node) => node.$('.price-tag-content__price-tag-price--current > .price-tag-price__euros').attr('content'),
+    parseName: (node) => node.$('.product__name-title').text(),
+  },
+};
+
+const productsToWatch = Object.keys(watchlist).reduce((l, x) => {
+  return [...l, ...watchlist[x].map(y => ({
+    id: y,
+    type: x,
+  }))];
+}, []);
+
+const crawler = new Crawler({
+  maxConnections : 10,
+  callback: () => { throw "No custom function defined" },
+});
+
+let crawledIndex = 0;
+
+const finishedCrawling = () => {
+  if (fs.existsSync('./db.json')) lastHistory = JSON.parse(fs.readFileSync('./db.json'));
+
+  if (lastHistory) {
+    for (let productId in lastHistory) {
+      const product = lastHistory[productId];
+      const timeDifference = Math.abs(new Date().getTime() - product.time) / (1000 * 3600 * 24);
+
+      if (!product.value && timeDifference >= 1 && !product.notified) {
+        const message = `Unable to get price for product [${productId}]`;
+        bot.sendMessage(process.env.TELEGRAM_CHAT_ID, message);
+
+        history[productId].notified = true;
+        console.log(message);
+        continue;
+      }
+
+      if (product.value && history[productId].value && product.value !== history[productId].value) {
+        const message = `Price has changed for product [${productId}] (${product.name}), ${product.value.toFixed(2)} -> ${history[productId].value.toFixed(2)}`;
+        bot.sendMessage(process.env.TELEGRAM_CHAT_ID, message);
+
+        console.log(message);
+        continue;
+      }
+    }
+  }
+
+  fs.writeFileSync('./db.json', JSON.stringify(history));
+
+  console.log(`Finished crawling [${crawledIndex}] products`);
+}
+
+for (let product of productsToWatch) {
+  const setting = settings[product.type];
+
+  if (!setting) throw "Unknown product type";
+
+  crawler.queue([{
+    uri: `${setting.baseUrl}${product.id}`,
+    jQuery: true,
+  
+    callback: (error, res, done) => {
+      let value = null;
+      let name = product.id;
+
+      if (!error) {
+        try {
+          value = parseFloat(setting.parsePrice(res));
+        } catch(e) {
+          value = null;
+        }
+
+        try {
+          name = setting.parseName(res);
+        } catch(e) {
+          name = null;
+        }
+      } else {
+        console.error(error);
+      }
+
+      history[`${product.type}-${product.id}`] = {
+        time: Date.now(),
+        name,
+        value,
+      };
+
+      crawledIndex++;
+
+      if (crawledIndex === productsToWatch.length) {
+        finishedCrawling();
+      }
+
+      done();
+    }
+  }]);
+}
